509ef3a7739c76af63f4a2cab929c9a6e92d99b2
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-16 23:59:49 +0300

    Storeg phonebook in localStoreg

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import ContactList from "./components/ContactList/ContactList";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem("phonebook");
+    const parsContacts = JSON.parse(localContacts);
+    if (parsContacts) {
+      this.setState({
+        contacts: parsContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("phonebook", JSON.stringify(this.state.contacts));
+    }
+  }
 
   handlerChange = (event) => {
     this.setState({
